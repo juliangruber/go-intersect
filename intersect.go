6004f902b0a90501b352f7be92fd63c0ff04a0fe
Soren Schwartz <s_schwartz@live.dk>
2022-07-18 12:27:55 +0200

    Moved files to top and complexity description to those not deprecated

--- intersect.go
+++ intersect.go
@@ -6,7 +6,62 @@ import (
 )
 
 // Simple has complexity: O(n^2)
-// Deprecated: Use SimpleGeneric instead.
+func SimpleGeneric[T comparable](a []T, b []T) []T {
+	set := make([]T, 0)
+
+	for _, v := range a {
+		if containsGeneric(b, v) {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
+
+// Sorted has complexity: O(n * log(n)), a needs to be sorted
+func SortedGeneric[T comparable](a []T, b []T) []T {
+	set := make([]T, 0)
+
+	for _, v := range a {
+		idx := sort.Search(len(b), func(i int) bool {
+			return b[i] == v
+		})
+		if idx < len(b) && b[idx] == v {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
+
+// Hash has complexity: O(n * x) where x is a factor of hash function efficiency (between 1 and 2)
+func HashGeneric[T comparable](a []T, b []T) []T {
+	set := make([]T, 0)
+	hash := make(map[T]bool)
+
+	for _, v := range a {
+		hash[v] = true
+	}
+
+	for _, v := range b {
+		if hash[v] {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
+
+func containsGeneric[T comparable](b []T, e T) bool {
+	for _, v := range b {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
+
+// Deprecated: Use SimpleGeneric instead. Complexity same as SimpleGeneric.
 func Simple(a interface{}, b interface{}) []interface{} {
 	set := make([]interface{}, 0)
 	av := reflect.ValueOf(a)
@@ -21,21 +76,7 @@ func Simple(a interface{}, b interface{}) []interface{} {
 	return set
 }
 
-// Same as above simple using generics - requires go v1.18
-func SimpleGeneric[T comparable](a []T, b []T) []T {
-	set := make([]T, 0)
-
-	for _, v := range a {
-		if containsGeneric(b, v) {
-			set = append(set, v)
-		}
-	}
-
-	return set
-}
-
-// Sorted has complexity: O(n * log(n)), a needs to be sorted
-// Deprecated: Use SortedGeneric instead.
+// Deprecated: Use SortedGeneric instead. Complexity same as SortedGeneric.
 func Sorted(a interface{}, b interface{}) []interface{} {
 	set := make([]interface{}, 0)
 	av := reflect.ValueOf(a)
@@ -54,24 +95,7 @@ func Sorted(a interface{}, b interface{}) []interface{} {
 	return set
 }
 
-// Same as above sorted using generics - requires go v1.18
-func SortedGeneric[T comparable](a []T, b []T) []T {
-	set := make([]T, 0)
-
-	for _, v := range a {
-		idx := sort.Search(len(b), func(i int) bool {
-			return b[i] == v
-		})
-		if idx < len(b) && b[idx] == v {
-			set = append(set, v)
-		}
-	}
-
-	return set
-}
-
-// Hash has complexity: O(n * x) where x is a factor of hash function efficiency (between 1 and 2)
-// Deprecated: Use HashGeneric instead.
+// Deprecated: Use HashGeneric instead. Complexity same as HashGeneric.
 func Hash(a interface{}, b interface{}) []interface{} {
 	set := make([]interface{}, 0)
 	hash := make(map[interface{}]bool)
@@ -93,38 +117,12 @@ func Hash(a interface{}, b interface{}) []interface{} {
 	return set
 }
 
-// Same as above hash using generics - requires go v1.18
-func HashGeneric[T comparable](a []T, b []T) []T {
-	set := make([]T, 0)
-	hash := make(map[T]bool)
-
-	for _, v := range a {
-		hash[v] = true
-	}
-
-	for _, v := range b {
-		if hash[v] {
-			set = append(set, v)
-		}
-	}
-
-	return set
-}
-
+// Deprecated: Used by Simple which uses reflection.
 func contains(a interface{}, e interface{}) bool {
 	v := reflect.ValueOf(a)
 
 	for i := 0; i < v.Len(); i++ {
 		if v.Index(i).Interface() == e {
-			return true
-		}
-	}
-	return false
-}
-
-func containsGeneric[T comparable](b []T, e T) bool {
-	for _, v := range b {
-		if v == e {
 			return true
 		}
 	}
